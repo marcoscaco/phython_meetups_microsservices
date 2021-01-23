ce5981f295b16f1570dc47b5c055e015668436fd
Matheus Lima <matheus.correa@mindtek.com.br>
2021-01-23 11:41:37 -0300

    usando o pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,17 @@
 import pytest
 
+
 def test_lambda_handler():
     from src.main import lambda_handler
-    
 
     event = None
     context = None
 
-    handler_return = lambda_handler(event,context)
-    expected_return = "Hello World"
-    assert isinstance(handler_return, str)
+    handler_return = lambda_handler(event, context)
+    expected_return = 3
+
+    #### Começa os testes
+
+    assert isinstance(handler_return, int)
     assert handler_return == expected_return
+
