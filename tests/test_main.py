b8c0ba15a1749affb661eec8923b974e6e49f555
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2021-01-23 11:17:53 -0300

    Estrutura basica do lambda + teste basico

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_lambda_handler():
+    from src.main import lambda_handler
+
+    event = None
+    context = None
+
+    handler_return = lambda_handler(event, context)
+    expected_return = "Hello"
+
+    #### Começa os testes
+
+    assert isinstance(handler_return, str)
+    assert handler_return == expected_return
+
