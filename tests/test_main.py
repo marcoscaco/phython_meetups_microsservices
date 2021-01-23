18bd793f923e5530e33b9a3bfe03249176afa345
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2021-01-23 19:41:29 -0300

    lint

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_lambda_handler():
     from src.main import lambda_handler
 
@@ -10,8 +7,7 @@ def test_lambda_handler():
     handler_return = lambda_handler(event, context)
     expected_return = "Hello"
 
-    #### Começa os testes
-
+    # Os testes
     assert isinstance(handler_return, str)
     assert handler_return == expected_return
 
@@ -20,8 +16,6 @@ def test_number_par():
     from src.main import number_par
 
     handler_return = number_par()
-    expected_return = bool(handler_return%2==0)
+    expected_return = bool(handler_return % 2 == 0)
 
-    assert expected_return == True
-
-
+    assert expected_return
