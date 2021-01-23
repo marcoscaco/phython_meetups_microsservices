50f95fdd09765be20a81743a3ee79bd9666e44be
Lucenildo <lucecerqueira@gmail.com>
2021-01-23 11:59:24 -0300

    novo teste dia 23

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_lambda_handler():
-    from src.main import lambda_handler
+    from python_meetups_microsservices.src.main import lambda_handler
 
     event = None
     context = None
